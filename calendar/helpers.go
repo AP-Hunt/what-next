ff200b0ee00b4c5f9fb8c1ff3bd866fcd56747b7
Andy Hunt <git@andyhunt.me>
2022-09-29 19:43:23 +0100

    Determine start and end times correctly
    According to RFC 5545, the actual start and end time of an event depends
    upon the data type of DTSTART, and the presence of DTEND and DURATION.
    
    https://www.rfc-editor.org/rfc/rfc5545#section-3.6.1
          The "DTSTART" property for a "VEVENT" specifies the inclusive
          start of the event.  For recurring events, it also specifies the
          very first instance in the recurrence set.  The "DTEND" property
          for a "VEVENT" calendar component specifies the non-inclusive end
          of the event.  For cases where a "VEVENT" calendar component
          specifies a "DTSTART" property with a DATE value type but no
          "DTEND" nor "DURATION" property, the event's duration is taken to
          be one day.  For cases where a "VEVENT" calendar component
          specifies a "DTSTART" property with a DATE-TIME value type but no
          "DTEND" property, the event ends on the same calendar date and
          time of day specified by the "DTSTART" property.

--- calendar/helpers.go
+++ calendar/helpers.go
@@ -3,14 +3,20 @@ package calendar
 import (
 	"fmt"
 	"regexp"
+	"strings"
 	"time"
 
 	ical "github.com/arran4/golang-ical"
+	"github.com/golang-module/carbon/v2"
 	"golang.org/x/exp/slices"
 )
 
 var midnightToday = time.Now().Truncate(24 * time.Hour)
 var midnightTomorrow = midnightToday.Add(24 * time.Hour)
+
+// Date format as defined in RFC 5545
+// https://www.rfc-editor.org/rfc/rfc5545#section-3.3.4
+var RegexIcalDate *regexp.Regexp = regexp.MustCompile("^[0-9]{4}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])$")
 
 func EventStartsToday(evt *ical.VEvent) (bool, error) {
 	start, err := evt.GetStartAt()
@@ -52,28 +58,78 @@ func EventIsCurrentlyHappening(evt *ical.VEvent, now time.Time) (bool, error) {
 }
 
 func EventStartAndEnd(evt *ical.VEvent) (time.Time, time.Time, error) {
-	var err error = nil
-	start, startErr := evt.GetStartAt()
-	if startErr != nil {
-		err = fmt.Errorf("cannot fetch start time: %s", startErr)
+	tZero := time.Time{}
+	DTSTART := evt.GetProperty(ical.ComponentPropertyDtStart)
+	DTEND := evt.GetProperty(ical.ComponentPropertyDtEnd)
+	DURATION := evt.GetProperty("DURATION")
+
+	if DTSTART == nil {
+		// -DSTART
+		return tZero, tZero, fmt.Errorf("no start time specified")
+	} else if DTSTART != nil && DTEND != nil {
+		// DTSTART + DTEND
+
+		var err error = nil
+		start, startErr := evt.GetStartAt()
+		if startErr != nil {
+			err = fmt.Errorf("cannot fetch start time: %s", startErr)
+		}
+
+		end, endErr := evt.GetEndAt()
+		if endErr != nil {
+			// The specification in RFC 5545 says that an event
+			// with a DTSTART of type DATE-TIME and no DTEND
+			// should be interpreted as starting and ending at
+			// the same time
+			// https://www.rfc-editor.org/rfc/rfc5545#section-3.6.1
+			end = start
+		}
+
+		return start, end, err
+	} else if DTSTART != nil && (DTEND == nil && DURATION == nil) {
+		// DTSRT - DTEND - DURATION
+
+		startIsDate := RegexIcalDate.Match([]byte(DTSTART.Value))
+		if startIsDate {
+			start, err := evt.GetAllDayStartAt()
+			if err != nil {
+				return tZero, tZero, err
+			}
+
+			// When DTSTART is present with type of DATE
+			// but there is no DTEND or DURATION, the end
+			// time is the start of the next day
+			end := carbon.Time2Carbon(start).Tomorrow().StartOfDay().Carbon2Time()
+			return start, end, nil
+		} else {
+			start, err := evt.GetStartAt()
+			if err != nil {
+				return tZero, tZero, err
+			}
+
+			// When DTSTART is present with type of DATETIME
+			// but there is no DTEND or DURATION, the end time
+			// is the same as the start time
+			return start, start, nil
+		}
+	} else if DTSTART != nil && DURATION != nil {
+		// DTSTART + DURATION
+
+		duration, err := time.ParseDuration(strings.ToLower(DURATION.Value))
+		if err != nil {
+			return tZero, tZero, fmt.Errorf("parsing duration: %s", err)
+		}
+
+		start, err := evt.GetStartAt()
+		if err != nil {
+			return tZero, tZero, err
+		}
+
+		return start, start.Add(duration), nil
 	}
 
-	end, endErr := evt.GetEndAt()
-	if endErr != nil {
-		// The specification in RFC 5545 says that an event
-		// with VEVENT with a DATE-TIME type DTSTART and no
-		// DTEND should be interpreted as starting and ending
-		// at the same time
-		// https://www.rfc-editor.org/rfc/rfc5545#section-3.6.1
-		end = start
-	}
-
-	return start, end, err
+	return tZero, tZero, nil
 }
-
-// Date format as defined in RFC 5545
-// https://www.rfc-editor.org/rfc/rfc5545#section-3.3.4
-var RegexIcalDate *regexp.Regexp = regexp.MustCompile("^[0-9]{4}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])$")
 
 func IsAllDayEvent(evt *ical.VEvent) (bool, error) {
 	DTSTART := evt.GetProperty(ical.ComponentPropertyDtStart)
--- calendar/helpers_test.go
+++ calendar/helpers_test.go
@@ -1,6 +1,7 @@
 package calendar_test
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/AP-Hunt/what-next/m/calendar"
@@ -8,6 +9,28 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+func fmtDATETIME(t time.Time) string {
+	return fmt.Sprintf(
+		"%d%02d%02dT%02d%02d%02d%s",
+		t.Year(),
+		t.Month(),
+		t.Day(),
+		t.Hour(),
+		t.Minute(),
+		t.Second(),
+		t.Format("Z"),
+	)
+}
+
+func fmtDATE(t time.Time) string {
+	return fmt.Sprintf(
+		"%0d%02d%02d",
+		t.Year(),
+		t.Month(),
+		t.Day(),
+	)
+}
 
 var _ = Describe("Helpers", func() {
 	midnightToday := time.Now().Truncate(24 * time.Hour)
@@ -107,13 +130,15 @@ var _ = Describe("Helpers", func() {
 	})
 
 	Describe("EventStartAndEnd", func() {
-		Context("when an event has a proper start and end time", func() {
-			It("will return those", func() {
+
+		Context("when an event has a proper start and end time of type DATETIME", func() {
+			It("will return those times", func() {
 				evt := ical.NewEvent("evt")
-				start := time.Now().Add(-10 * time.Minute).UTC()
-				evt.SetStartAt(start)
-				end := time.Now().Add(10 * time.Minute).UTC()
-				evt.SetEndAt(end)
+				start := time.Date(2020, 01, 30, 12, 20, 00, 00, time.UTC)
+				evt.SetProperty(ical.ComponentPropertyDtStart, fmtDATETIME(start))
+
+				end := time.Date(2020, 01, 30, 12, 20, 00, 00, time.UTC)
+				evt.SetProperty(ical.ComponentPropertyDtEnd, fmtDATETIME(end))
 
 				actualStart, actualEnd, err := calendar.EventStartAndEnd(evt)
 				Expect(err).ToNot(HaveOccurred())
@@ -122,16 +147,55 @@ var _ = Describe("Helpers", func() {
 			})
 		})
 
-		Context("when an event has a start time but no end time", func() {
-			It("will return the start and end times as the same value, as per the spec", func() {
+		Context("when an event has a start time of type DATE but no end time or duration", func() {
+			It("will return the start as midnight of the date, and end as the midnight of the next day", func() {
 				evt := ical.NewEvent("evt")
-				start := time.Now().Add(-10 * time.Minute).UTC()
-				evt.SetStartAt(start)
+
+				start := time.Date(2020, 01, 30, 12, 20, 00, 00, time.UTC)
+				evt.SetProperty(ical.ComponentPropertyDtStart, fmtDATE(start))
+
+				expectedStart := time.Date(2020, 01, 30, 00, 00, 00, 00, time.UTC)
+				expectedEnd := time.Date(2020, 01, 31, 00, 00, 00, 0, time.UTC)
 
 				actualStart, actualEnd, err := calendar.EventStartAndEnd(evt)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(actualStart).To(BeTemporally("~", start, 1*time.Minute))
-				Expect(actualEnd).To(BeTemporally("~", start, 1*time.Minute))
+				Expect(actualStart).To(BeTemporally("~", expectedStart, 1*time.Minute))
+				Expect(actualEnd).To(BeTemporally("~", expectedEnd, 1*time.Minute))
+			})
+		})
+
+		Context("when an event has a start time of type DATETIME and a duration", func() {
+			It("will return the defined start time, and the end time as start + duration", func() {
+				evt := ical.NewEvent("evt")
+
+				start := time.Date(2020, 01, 30, 12, 20, 00, 00, time.UTC)
+				evt.SetProperty(ical.ComponentPropertyDtStart, fmtDATETIME(start))
+
+				evt.SetProperty("DURATION", "30M")
+
+				expectedStart := time.Date(2020, 01, 30, 12, 20, 00, 00, time.UTC)
+				expectedEnd := time.Date(2020, 01, 30, 12, 50, 00, 0, time.UTC)
+
+				actualStart, actualEnd, err := calendar.EventStartAndEnd(evt)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(actualStart).To(BeTemporally("~", expectedStart, 1*time.Minute))
+				Expect(actualEnd).To(BeTemporally("~", expectedEnd, 1*time.Minute))
+			})
+		})
+
+		Context("when an event has a start time of type DATETIME but no end time or duration", func() {
+			It("will return the start and end time as the same time", func() {
+				evt := ical.NewEvent("evt")
+
+				start := time.Date(2020, 01, 30, 12, 20, 00, 00, time.UTC)
+				evt.SetProperty(ical.ComponentPropertyDtStart, fmtDATETIME(start))
+
+				expectedStart := time.Date(2020, 01, 30, 12, 20, 00, 00, time.UTC)
+
+				actualStart, actualEnd, err := calendar.EventStartAndEnd(evt)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(actualStart).To(BeTemporally("~", expectedStart, 1*time.Minute))
+				Expect(actualEnd).To(BeTemporally("~", expectedStart, 1*time.Minute))
 			})
 		})
 	})
